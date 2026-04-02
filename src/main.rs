1927bdf2a3dbf468064696e5f96ee84349069cd9
ErmitaVulpe <emily.wilczek@proton.me>
2026-04-02 09:36:34 +0200

    Yippee it workie

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,51 @@
-use std::f32::consts::FRAC_PI_2;
-
 use bevy::prelude::*;
 use bevy_egui::{egui::Color32, prelude::*};
 use bevy_rapier2d::prelude::*;
+use derive_more::IsVariant;
 use rand::RngExt;
 
-const BALL_RADIUS: f32 = 10.;
-const BALL_RESTITUTION: Restitution = Restitution::coefficient(0.7);
+const BALL_RADIUS: f32 = 5.;
+const BALL_RESTITUTION: Restitution = Restitution::coefficient(0.2);
+const BALL_FRICTION: Friction = Friction::coefficient(1.5);
+const BALL_SPAWN_JITTER: f32 = 0.05;
 
-const PEG_RADIUS: f32 = 5.;
+const PEG_RADIUS: f32 = 10.;
 const PEG_RESTITUTION: Restitution = Restitution::coefficient(0.5);
+const PEG_FRICTION: Friction = Friction::coefficient(0.7);
 
-const PEG_HORIZONTAL_SPACING: f32 = 50.;
-const PEG_VERTICAL_SPACING: f32 = 50.;
+const WALL_RADIUS: f32 = PEG_RADIUS / 2.;
+const WALL_RESTITUTION: Restitution = Restitution::coefficient(1.);
+const WALL_FRICTION: Friction = Friction::coefficient(100.);
+
+const PEG_HORIZONTAL_SPACING: f32 = 80.;
+const PEG_VERTICAL_SPACING: f32 = 40.;
 const PEG_SPAWN_JITTER: f32 = 0.05;
-
-const PEG_CENTER_SPACING: f32 = 2. * PEG_RADIUS + PEG_HORIZONTAL_SPACING;
-const PEG_LAYER_SPACING: f32 = 2. * PEG_RADIUS + PEG_VERTICAL_SPACING;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin::default())
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1000.))
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(50.))
         .add_plugins(RapierDebugRenderPlugin::default())
         .init_resource::<RedrawBoard>()
         .init_resource::<PegLayers>()
         .init_resource::<NumberOfBalls>()
-        .add_systems(Startup, (setup_camera_system, setup_physics, setup_board))
+        .init_state::<SimState>()
+        .add_systems(Startup, (setup_camera_system, setup_board))
         .add_systems(EguiPrimaryContextPass, ui_system)
         .add_systems(
             Update,
             ((destroy_board, setup_board)
                 .chain()
                 .run_if(resource_equals::<RedrawBoard>(RedrawBoard(true))),),
+        )
+        .add_systems(FixedUpdate, spawn_balls.run_if(in_state(SimState::Running)))
+        .add_systems(OnEnter(SimState::NotRunning), destroy_balls)
+        .add_systems(
+            OnEnter(SimState::Running),
+            |mut commands: Commands, number_of_balls: Res<NumberOfBalls>| {
+                commands.insert_resource(BallsToSpawn(number_of_balls.0));
+            },
         )
         .run();
 }
@@ -42,25 +54,27 @@ fn setup_camera_system(mut commands: Commands) {
     commands.spawn((Camera2d, Transform::from_scale(Vec3::ONE * 3.)));
 }
 
-fn setup_physics(mut commands: Commands) {
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(BALL_RADIUS))
-        .insert(Ccd::enabled())
-        .insert(BALL_RESTITUTION)
-        .insert(Transform::from_xyz(0.0, 400.0, 0.0));
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(BALL_RADIUS))
-        .insert(Ccd::enabled())
-        .insert(BALL_RESTITUTION)
-        .insert(Transform::from_xyz(0.0, 420.0, 0.0));
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(BALL_RADIUS))
-        .insert(Ccd::enabled())
-        .insert(BALL_RESTITUTION)
-        .insert(Transform::from_xyz(0.0, 440.0, 0.0));
+#[derive(Debug, Component)]
+struct BallMarker;
+
+fn spawn_balls(mut commands: Commands, mut balls_to_spawn: ResMut<BallsToSpawn>) {
+    if balls_to_spawn.0 != 0 {
+        let mut rng = rand::rng();
+        let x_jitter = rng.random_range(-BALL_SPAWN_JITTER..=BALL_SPAWN_JITTER);
+        let y_jitter = rng.random_range(-BALL_SPAWN_JITTER..=BALL_SPAWN_JITTER);
+
+        commands.spawn((
+            BallMarker,
+            RigidBody::Dynamic,
+            Collider::ball(BALL_RADIUS),
+            Ccd::enabled(),
+            BALL_RESTITUTION,
+            BALL_FRICTION,
+            Transform::from_xyz(0. + x_jitter, 400. + y_jitter, 0.),
+        ));
+
+        balls_to_spawn.0 -= 1;
+    }
 }
 
 #[derive(Resource, Default, PartialEq, Eq)]
@@ -84,10 +98,22 @@ impl Default for NumberOfBalls {
     }
 }
 
+#[derive(Resource)]
+struct BallsToSpawn(u32);
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States, IsVariant)]
+enum SimState {
+    #[default]
+    NotRunning,
+    Running,
+}
+
 fn ui_system(
     mut redraw_board: ResMut<RedrawBoard>,
     mut peg_layers: ResMut<PegLayers>,
     mut number_of_balls: ResMut<NumberOfBalls>,
+    mut sim_state_next: ResMut<NextState<SimState>>,
+    sim_state: Res<State<SimState>>,
     mut contexts: EguiContexts,
     mut camera_transform: Single<&mut Transform, With<Camera>>,
 ) {
@@ -95,16 +121,33 @@ fn ui_system(
         egui::SidePanel::left("left_panel")
             .resizable(false)
             .show(ctx, |ui| {
-                ui.label(
-                    egui::RichText::new("Galton board")
-                        .color(Color32::WHITE)
-                        .heading(),
-                );
+                ui.horizontal(|ui| {
+                    ui.label(
+                        egui::RichText::new("Galton board")
+                            .color(Color32::WHITE)
+                            .heading(),
+                    );
+                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                        if sim_state.get().is_running() {
+                            if ui.button("Cancel").clicked() {
+                                sim_state_next.set(SimState::NotRunning);
+                            };
+                        } else {
+                            if ui.button("Start").clicked() {
+                                sim_state_next.set(SimState::Running);
+                            };
+                        }
+                    });
+                });
                 egui::Grid::new("settings").show(ui, |ui| {
+                    if sim_state.get().is_running() {
+                        ui.disable();
+                    }
+
                     // peg layers
                     ui.label("Number of peg layers");
                     redraw_board.0 = ui
-                        .add(egui::Slider::new(&mut peg_layers.0, 3..=30))
+                        .add(egui::Slider::new(&mut peg_layers.0, 3..=30).drag_value_speed(0.1))
                         .changed();
                     ui.end_row();
 
@@ -144,6 +187,25 @@ fn ui_system(
 #[derive(Component)]
 struct Board;
 
+#[derive(Debug, Bundle)]
+struct WallBundle {
+    collider: Collider,
+    friction: Friction,
+    restitution: Restitution,
+    ccd: Ccd,
+}
+
+impl WallBundle {
+    fn new(point1: Vec2, point2: Vec2) -> Self {
+        Self {
+            collider: Collider::capsule(point1, point2, WALL_RADIUS),
+            friction: WALL_FRICTION,
+            restitution: WALL_RESTITUTION,
+            ccd: Ccd::enabled(),
+        }
+    }
+}
+
 fn setup_board(mut commands: Commands, peg_layers: Res<PegLayers>) {
     let mut rng = rand::rng();
 
@@ -160,59 +222,61 @@ fn setup_board(mut commands: Commands, peg_layers: Res<PegLayers>) {
                     parent.spawn((
                         Collider::ball(PEG_RADIUS),
                         PEG_RESTITUTION,
+                        PEG_FRICTION,
                         Ccd::enabled(),
                         Transform::from_xyz(
-                            x_jitter + horizontal_offset_base + (i as f32) * PEG_CENTER_SPACING,
-                            y_jitter - (layer as f32) * PEG_LAYER_SPACING,
+                            x_jitter + horizontal_offset_base + (i as f32) * PEG_HORIZONTAL_SPACING,
+                            y_jitter - (layer as f32) * PEG_VERTICAL_SPACING,
                             0.,
                         ),
                     ));
                 }
-                horizontal_offset_base -= PEG_CENTER_SPACING / 2.;
+                horizontal_offset_base -= PEG_HORIZONTAL_SPACING / 2.;
             }
 
             // Spawn bucket walls
-            horizontal_offset_base -= PEG_CENTER_SPACING / 2.;
-            let bucket_wall_half_height = 200.;
-            let last_layer_y = ((peg_layers.0 - 1) as f32) * PEG_LAYER_SPACING;
-            let bucket_wall_center = -last_layer_y - bucket_wall_half_height;
+            horizontal_offset_base -= PEG_HORIZONTAL_SPACING / 2.;
+            let last_layer_y = -((peg_layers.0 - 1) as f32) * PEG_VERTICAL_SPACING;
             for i in 0..peg_layers.0 + 2 {
-                parent.spawn((
-                    Collider::cuboid(PEG_RADIUS * 0.5, bucket_wall_half_height),
-                    Ccd::enabled(),
-                    Transform::from_xyz(
-                        horizontal_offset_base + (i as f32) * PEG_CENTER_SPACING,
-                        bucket_wall_center,
-                        0.,
+                parent.spawn(WallBundle::new(
+                    Vec2::new(
+                        horizontal_offset_base + (i as f32) * PEG_HORIZONTAL_SPACING,
+                        last_layer_y,
+                    ),
+                    Vec2::new(
+                        horizontal_offset_base + (i as f32) * PEG_HORIZONTAL_SPACING,
+                        last_layer_y - 400., // TODO parametrise this bih
                     ),
                 ));
             }
 
+            // Spawn bucket floor
+            let floor_left_point = Vec2::new(horizontal_offset_base, last_layer_y - 400.);
+            parent.spawn(WallBundle::new(floor_left_point, flip_x(floor_left_point)));
+
             // Spawn side barriers
-            let point1 = Vec2::from((horizontal_offset_base, -last_layer_y));
+            fn flip_x(v: Vec2) -> Vec2 {
+                v * Vec2::new(-1.0, 1.0)
+            }
+
+            let point1 = Vec2::from((horizontal_offset_base, last_layer_y));
             let point2 = Vec2::from((-PEG_HORIZONTAL_SPACING / 2., PEG_VERTICAL_SPACING));
-            let half_distance = point1.distance(point2) / 2.;
-            let midpoint = point1.midpoint(point2);
-            let angle = (point1 - point2).to_angle();
-            let mut transform = Transform::from_translation(midpoint.extend(0.));
-            transform.rotate_z(angle + FRAC_PI_2);
-            parent.spawn((
-                Collider::cuboid(PEG_RADIUS * 0.5, half_distance),
-                PEG_RESTITUTION,
-                Ccd::enabled(),
-                transform,
-            ));
-            transform.translation.x = -transform.translation.x;
-            transform.rotate_z(-2. * (angle + FRAC_PI_2));
-            parent.spawn((
-                Collider::cuboid(PEG_RADIUS * 0.5, half_distance),
-                PEG_RESTITUTION,
-                Ccd::enabled(),
-                transform,
-            ));
+            parent.spawn(WallBundle::new(point1, point2));
+            parent.spawn(WallBundle::new(flip_x(point1), flip_x(point2)));
+
+            // Spawn the spawn area
+            let bucket_point = Vec2::from((-300., 175.));
+            parent.spawn(WallBundle::new(point2, bucket_point));
+            parent.spawn(WallBundle::new(flip_x(point2), flip_x(bucket_point)));
         });
 }
 
 fn destroy_board(mut commands: Commands, query: Single<Entity, With<Board>>) {
     commands.entity(*query).despawn();
+}
+
+fn destroy_balls(mut commands: Commands, query: Query<Entity, With<BallMarker>>) {
+    for ball in query {
+        commands.entity(ball).despawn();
+    }
 }
