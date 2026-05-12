ea8cba5364f1af298327d2af0ae5e576c585e364
ErmitaVulpe <emily.wilczek@proton.me>
2026-05-12 23:59:21 +0200

    Added board scaling to viewport

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use bevy::prelude::*;
+use bevy::{
+    math::bounding::{Aabb2d, BoundingVolume},
+    prelude::*,
+    window::WindowResized,
+};
 use bevy_egui::{egui::Color32, prelude::*};
 use bevy_rapier2d::prelude::*;
 use bevy_svg::prelude::*;
@@ -25,6 +29,10 @@ const WALL_RESTITUTION: Restitution = Restitution::coefficient(0.5);
 const WALL_FRICTION: Friction = Friction::coefficient(0.075);
 const WALL_COLOR: Color = Color::srgb_u8(0xA0, 0xA0, 0xA0);
 
+const SPAWN_AREA_HEIGHT: f32 = 400.;
+
+const BOARD_CENTRERING_PADDING: f32 = 50.;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -37,26 +45,33 @@ fn main() {
         .add_plugins(EguiPlugin::default())
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(50.))
         .add_plugins(SvgPlugin)
-        .init_resource::<RedrawBoard>()
-        .init_resource::<PegLayers>()
         .init_resource::<NumberOfBalls>()
+        .init_resource::<PegLayers>()
+        .init_resource::<RedrawBoard>()
+        .init_resource::<SidePanelWidth>()
         .init_state::<SimState>()
         .add_systems(
             Startup,
-            (
-                setup_camera_system,
-                load_assets,
-                setup_board.after(load_assets),
-            ),
+            ((setup_camera_system, load_assets), setup_board).chain(),
         )
+        .add_systems(PostStartup, update_camera_system.after(setup_camera_system))
         .add_systems(EguiPrimaryContextPass, ui_system)
         .add_systems(
             Update,
-            ((destroy_board, setup_board)
-                .chain()
-                .run_if(resource_equals::<RedrawBoard>(RedrawBoard(true))),),
+            (
+                (destroy_board, setup_board, (update_camera_system))
+                    .chain()
+                    .run_if(resource_equals::<RedrawBoard>(RedrawBoard(true))),
+                update_camera_system
+                    .run_if(resource_changed::<SidePanelWidth>.or(on_message::<WindowResized>)),
+            ),
         )
-        .add_systems(FixedUpdate, spawn_balls.run_if(in_state(SimState::Running)))
+        .add_systems(
+            FixedUpdate,
+            spawn_balls
+                .before(PhysicsSet::StepSimulation)
+                .run_if(in_state(SimState::Running)),
+        )
         .add_systems(OnEnter(SimState::NotRunning), destroy_balls)
         .add_systems(
             OnEnter(SimState::Running),
@@ -68,7 +83,24 @@ fn main() {
 }
 
 fn setup_camera_system(mut commands: Commands) {
-    commands.spawn((Camera2d, Transform::from_scale(Vec3::ONE * 3.)));
+    commands.spawn((Camera2d, Transform::IDENTITY));
+}
+
+fn update_camera_system(
+    mut camera: Single<(&Camera, &mut Transform)>,
+    board_bounding_box: Single<&BoardBoundingBox>,
+    side_panel_width: Res<SidePanelWidth>,
+) {
+    let mut viewport_size = camera.0.logical_viewport_size().unwrap();
+    viewport_size.x -= side_panel_width.0;
+    let aabb = dbg!(&board_bounding_box.0);
+
+    let center = dbg!(aabb.center());
+    let size = dbg!(aabb.max - aabb.min);
+    let padded_size = dbg!(size + Vec2::splat(BOARD_CENTRERING_PADDING * 2.0));
+
+    camera.1.scale = Vec3::splat(dbg!((padded_size / viewport_size).max_element()));
+    camera.1.translation.y = dbg!(center.y);
 }
 
 #[derive(Debug, Component)]
@@ -112,12 +144,15 @@ fn spawn_balls(
 #[derive(Resource, Default, PartialEq, Eq)]
 struct RedrawBoard(bool);
 
+#[derive(Resource, Default)]
+struct SidePanelWidth(f32);
+
 #[derive(Resource)]
 struct PegLayers(u8);
 
 impl Default for PegLayers {
     fn default() -> Self {
-        Self(7)
+        Self(15)
     }
 }
 
@@ -140,10 +175,15 @@ enum SimState {
     Running,
 }
 
+#[allow(
+    clippy::too_many_arguments,
+    reason = "Bevy requires this many to function"
+)]
 fn ui_system(
     mut redraw_board: ResMut<RedrawBoard>,
     mut peg_layers: ResMut<PegLayers>,
     mut number_of_balls: ResMut<NumberOfBalls>,
+    mut side_panel_width: ResMut<SidePanelWidth>,
     mut sim_state_next: ResMut<NextState<SimState>>,
     sim_state: Res<State<SimState>>,
     mut contexts: EguiContexts,
@@ -151,12 +191,18 @@ fn ui_system(
 ) {
     if let Ok(ctx) = contexts.ctx_mut() {
         egui::SidePanel::left("left_panel")
-            .resizable(false)
+            .min_width(420.)
+            .max_width(600.)
             .show(ctx, |ui| {
                 // Move the camera off so its centered in the area remaining from the side panel
                 let max_rect = ui.max_rect();
+                let side_panel_width_ref = &side_panel_width;
+                let new_side_panel_width = max_rect.max.x + max_rect.min.x;
                 camera_transform.translation.x =
-                    (max_rect.max.x + max_rect.min.x) * camera_transform.scale.x / -2.;
+                    new_side_panel_width * camera_transform.scale.x / -2.;
+                if side_panel_width_ref.0 != new_side_panel_width {
+                    side_panel_width.0 = new_side_panel_width;
+                }
 
                 ui.horizontal(|ui| {
                     ui.label(
@@ -184,7 +230,7 @@ fn ui_system(
                     // peg layers
                     ui.label("Number of peg layers");
                     redraw_board.0 = ui
-                        .add(egui::Slider::new(&mut peg_layers.0, 3..=30).drag_value_speed(0.1))
+                        .add(egui::Slider::new(&mut peg_layers.0, 10..=50).drag_value_speed(0.1))
                         .changed();
                     ui.end_row();
 
@@ -200,7 +246,11 @@ fn ui_system(
                         if ui.button("-1").clicked() {
                             number_of_balls.0 = number_of_balls.0.saturating_sub(1);
                         }
-                        ui.add(egui::DragValue::new(&mut number_of_balls.0).speed(1));
+                        ui.add(
+                            egui::DragValue::new(&mut number_of_balls.0)
+                                .speed(1)
+                                .range(1..=10_000_000),
+                        );
                         if ui.button("+1").clicked() {
                             number_of_balls.0 = number_of_balls.0.saturating_add(1);
                         }
@@ -219,6 +269,9 @@ fn ui_system(
 
 #[derive(Component)]
 struct Board;
+
+#[derive(Component, Constructor)]
+struct BoardBoundingBox(Aabb2d);
 
 /// Convenience struct for generating multiple walls
 #[derive(Constructor)]
@@ -273,6 +326,7 @@ impl WallBundle {
     }
 }
 
+// This whole function is a warcrime
 fn setup_board(
     mut commands: Commands,
     peg_layers: Res<PegLayers>,
@@ -280,6 +334,7 @@ fn setup_board(
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
     let mut rng = rand::rng();
+    let mut board_bounding_box = Aabb2d::new(Default::default(), Default::default());
 
     commands
         .spawn((Board, Transform::default(), Visibility::Visible))
@@ -313,6 +368,7 @@ fn setup_board(
             // Spawn bucket walls
             horizontal_offset_base -= PEG_HORIZONTAL_SPACING / 2.;
             let last_layer_y = -((peg_layers.0 - 1) as f32) * PEG_VERTICAL_SPACING;
+            let bucket_floor_y = last_layer_y - 400.;
             for i in 0..peg_layers.0 + 2 {
                 parent.spawn(wall_factory.wall(
                     Vec2::new(
@@ -321,10 +377,15 @@ fn setup_board(
                     ),
                     Vec2::new(
                         horizontal_offset_base + (i as f32) * PEG_HORIZONTAL_SPACING,
-                        last_layer_y - 400., // TODO parametrise this bih
+                        bucket_floor_y,
                     ),
                 ));
             }
+
+            let bounding_box_bot_left = Vec2::new(
+                horizontal_offset_base - WALL_RADIUS,
+                bucket_floor_y - WALL_RADIUS,
+            );
 
             // Spawn bucket floor
             let floor_left_point = Vec2::new(horizontal_offset_base, last_layer_y - 400.);
@@ -340,11 +401,32 @@ fn setup_board(
             parent.spawn(wall_factory.wall(point1, point2));
             parent.spawn(wall_factory.wall(flip_x(point1), flip_x(point2)));
 
-            // Spawn the spawn area
+            // Spawn the spawn area floor
             let bucket_point = Vec2::from((-300., 175.));
             parent.spawn(wall_factory.wall(point2, bucket_point));
             parent.spawn(wall_factory.wall(flip_x(point2), flip_x(bucket_point)));
-        });
+
+            // Spawn the spawn area walls
+            let bucket_ceil_point1 = Vec2 {
+                x: bucket_point.x,
+                y: bucket_point.y + SPAWN_AREA_HEIGHT,
+            };
+            let bucket_ceil_point2 = flip_x(bucket_ceil_point1);
+            parent.spawn(wall_factory.wall(bucket_point, bucket_ceil_point1));
+            parent.spawn(wall_factory.wall(flip_x(bucket_point), bucket_ceil_point2));
+
+            // Spawn the spawn area ceiling
+            parent.spawn(wall_factory.wall(bucket_ceil_point1, bucket_ceil_point2));
+
+            let bounding_box_top_right =
+                Vec2::new(-bounding_box_bot_left.x, bucket_ceil_point1.y + WALL_RADIUS);
+
+            board_bounding_box = Aabb2d {
+                min: bounding_box_bot_left,
+                max: bounding_box_top_right,
+            };
+        })
+        .insert(BoardBoundingBox::new(board_bounding_box));
 }
 
 fn destroy_board(mut commands: Commands, query: Single<Entity, With<Board>>) {
